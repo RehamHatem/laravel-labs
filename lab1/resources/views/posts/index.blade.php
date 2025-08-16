c43d378592c3322ecae9b7d0e9db1b754ec3bdcc
reham hatem <rha772201@gmail.com>
2025-08-16 13:50:02 +0300

    show alert

--- lab1/resources/views/posts/index.blade.php
+++ lab1/resources/views/posts/index.blade.php
@@ -4,6 +4,11 @@
 
 @section('content')
 <div class="container mx-auto mt-2">
+    @if (session('success'))
+        <div class="mb-4 px-4 py-2 rounded-lg bg-green-100 text-green-800 border border-green-300">
+            {{ session('success') }}
+        </div>
+    @endif
     <div class="flex justify-center items-center ">
     <a href="{{ route('posts.create') }}" 
        class="bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded">
